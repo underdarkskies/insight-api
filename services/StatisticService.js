d85d3f945ebd50cd4bcd18bfb79691c2d70833de
TheTrunk <tadeas.kmenta@zel.cash>
2019-02-20 21:20:14 +0100

    fix null paddress

--- services/StatisticService.js
+++ services/StatisticService.js
@@ -1275,9 +1275,11 @@ StatisticService.prototype.getBlockRewardr = function (height) {
 
 StatisticService.prototype.getPoolInfo = function (paddress) {
     for (var k in this.poolStrings) {
-        if (paddress.toString().match(k)) {
-            this.poolStrings[k].address = paddress;
-            return this.poolStrings[k];
+        if (paddress != null) {
+            if (paddress.toString().match(k)) {
+                this.poolStrings[k].address = paddress;
+                return this.poolStrings[k];
+            }
         }
     }
     return {};
